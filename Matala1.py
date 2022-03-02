9a4ad87e8675f7cb427d56e7c692c9aa4282e5b8
AvielM95 <100766755+AvielM95@users.noreply.github.com>
2022-03-02 17:15:51 +0200

    Add files via upload

---
+++ Matala1.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""           
+
+#%%
+def my_func(x1,x2,x3):
+    
+      
+        if x1+x2+x3 == 0 :
+              return("Not a number-denominator is zero")
+                
+        elif  type(x1)!=float :  
+              return "ERROR:parameters should be float"
+              
+        elif  type(x2)!=float:  
+              return"ERROR:parameters should be float"
+        
+        elif  type(x3)!=float :  
+              return"ERROR:parameters should be float"
+        
+        else :
+             return((x1+x2+x3)*(x2+x3)*x3)/(x1+x2+x3)
+
+                                       
+print(my_func(1.0,1.0,-3))
+
+
+
+#%%
+
+
+def convert(hours=0,minute=0):
+    
+    if hours < 0 or minute < 0 :
+       return("Input error!")
+     
+    else:
+       return(hours*3600+minute*60)
+
+
+print(convert(1,3.0))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
